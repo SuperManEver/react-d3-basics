c3a93bf5ec855c16017a6b2af12e32996decb41e
Nik Luparev <nikita.luparev@gmail.com>
2022-03-23 01:19:03 +0500

    add scales

--- src/components/line-chart/index.jsx
+++ src/components/line-chart/index.jsx
@@ -17,17 +17,41 @@ class LineChart extends Component {
   }
 
   componentDidMount() {
-    let svg = d3
+    const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+      width = WIDTH - margin.left - margin.right,
+      height = HEIGHT - margin.top - margin.bottom;
+
+    const svg = d3
       .select(this.myRef.current)
       .append("svg")
-      .attr("width", WIDTH)
-      .attr("height", HEIGHT);
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Add X axis --> it is a date format
+    const xExtent = d3.extent(this.points, (d) => d.timestamp);
+
+    const x = d3.scaleTime().domain(xExtent).range([0, width]);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(this.points, (d) => d.price)])
+      .range([height, 0]);
+
+    svg.append("g").call(d3.axisLeft(y));
 
     svg
       .append("path")
       .datum(this.points)
       .attr("fill", "none")
-      .attr("stroke", "steelblue")
+      .attr("stroke", "red")
       .attr("stroke-width", 1.5)
       .attr("d", this.chartLine);
   }
@@ -35,7 +59,7 @@ class LineChart extends Component {
   get points() {
     return points.map((t) => ({
       ...t,
-      timestamp: new Date(t.timestamp),
+      timestamp: new Date(t.timestamp * 1000),
     }));
   }
 
